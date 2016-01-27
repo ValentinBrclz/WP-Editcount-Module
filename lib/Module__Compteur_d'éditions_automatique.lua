56b39210f66766b4a28f0987048dc505f039c180
Valentin Brclz <ValentinBrclz@users.noreply.github.com>
2016-01-27 05:22:23 +0100

    Using 1st level

--- lib/Module__Compteur_d'éditions_automatique.lua
+++ lib/Module__Compteur_d'éditions_automatique.lua
@@ -10,12 +10,12 @@ local p = {}
 function p.getEditCount(frame)
 	-- Load and store the bot-updated data
 	local data = mw.loadData( 'Module:Compteur d\'éditions automatique/data' )
-	-- Get the 2nd level arguments
-	local args = frame:getParent().args
-	-- If there is a value for the given user, return it
-	if data[args[1]] ~= nil then
-		return data[args[1]]
-	end
+	-- Get the 1st level arguments
+	local args = frame.args
+    -- If there is a value for the given user, return it
+    if data[args[1]] ~= nil then
+        return data[args[1]]
+    end
 	-- Elsewise just display a "?" until the bot get throught it
 	return "?"
 end
