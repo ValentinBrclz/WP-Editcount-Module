4be2951d6b205a2eef24b53e595671ad46435e05
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2016-01-27 01:52:21 +0100

    creation of the module

--- lib/Module__Compteur_d'éditions_automatique.lua
+++ lib/Module__Compteur_d'éditions_automatique.lua
@@ -1,8 +1,12 @@
--- Base from documentation
 local p = {}
 
-function p.hello( frame )
-    return "Hello, world!"
+function p.getEditCount(frame)
+	local data = mw.loadData( 'Module:Compteur d\'éditions automatique/data' )
+	args = frame:getParent().args
+	if data[args[1]] ~= nil then
+		return data[args[1]]
+	end
+	return "?"
 end
 
 return p
