5deec688e3932b66e04c40cd276cd1df70942442
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2016-01-27 01:56:45 +0100

    + comments

--- lib/Module__Compteur_d'éditions_automatique.lua
+++ lib/Module__Compteur_d'éditions_automatique.lua
@@ -1,11 +1,15 @@
 local p = {}
 
 function p.getEditCount(frame)
+	-- Load and store the bot-updated data
 	local data = mw.loadData( 'Module:Compteur d\'éditions automatique/data' )
+	-- Get the 2nd level arguments
 	args = frame:getParent().args
+	-- If there is a value for the given user, return it
 	if data[args[1]] ~= nil then
 		return data[args[1]]
 	end
+	-- Elsewise just display a "?" until the bot get throught it
 	return "?"
 end
 
