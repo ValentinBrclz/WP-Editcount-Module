377b3a75797ebd31b8b562db822d1fe2675a973a
Valentin Brclz <ValentinBrclz@users.noreply.github.com>
2016-01-27 02:41:02 +0100

    Making var "args" local as recommended in documentation

--- lib/Module__Compteur_d'éditions_automatique.lua
+++ lib/Module__Compteur_d'éditions_automatique.lua
@@ -11,7 +11,7 @@ function p.getEditCount(frame)
 	-- Load and store the bot-updated data
 	local data = mw.loadData( 'Module:Compteur d\'éditions automatique/data' )
 	-- Get the 2nd level arguments
-	args = frame:getParent().args
+	local args = frame:getParent().args
 	-- If there is a value for the given user, return it
 	if data[args[1]] ~= nil then
 		return data[args[1]]
